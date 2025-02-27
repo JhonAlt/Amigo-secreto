308a0a4106b489fe705a27ebc220457bd668677a
Jonathan Amézquita <jamezquita150@gmail.com>
2025-02-27 09:22:36 -0600

    Agregando función de lista

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = []
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value
+    if (nombreAmigo === "") {//Si el nombre del amigo está vacío, muestra un mensaje.
+        alert("Por favor, ingresa un nombre.")
+        return
+    }
+    listaAmigos.push(nombreAmigo)//Agrega el nombre del amigo a la lista.
+    document.getElementById("amigo").value = ""//Limpia el cuadro de texto.
+
+    console.log(listaAmigos)
+}
+
