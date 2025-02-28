5d5787d1172ac29e14e35f9b2ce13f08b46f4e4d
Jonathan Amézquita <jamezquita150@gmail.com>
2025-02-28 01:56:38 -0600

    Agregando función para actualizar la lista

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = []
+let nombres = []
 
 function agregarAmigo() {
-    let nombreAmigo = document.getElementById("amigo").value
+    let nombreAmigo = document.getElementById("amigo").value //Obtiene el nombre del amigo ingresado.
     if (nombreAmigo === "") {//Si el nombre del amigo está vacío, muestra un mensaje.
-        alert("Por favor, ingresa un nombre.")
-        return
+        alert("Por favor, ingresa un nombre.");
+        return 
     }
-    listaAmigos.push(nombreAmigo)//Agrega el nombre del amigo a la lista.
+    nombres.push(nombreAmigo)//Agrega el nombre del amigo a la lista.
     document.getElementById("amigo").value = ""//Limpia el cuadro de texto.
 
-    console.log(listaAmigos)
+
+    mostrarLista("listaAmigos", nombres)//Muestra la lista de amigos en el documento.
 }
 
+
+function mostrarLista(lista, elementos) {
+    let mostrarAmigos = document.getElementById(lista); 
+
+    mostrarAmigos.innerHTML = ""//Limpia el contenido de la lista.
+
+    for (let i = 0; i < elementos.length; i++) {//Recorre la lista de elementos.
+        let elementoLista = document.createElement("li");//Crea un elemento de lista.
+        elementoLista.textContent = elementos[i];//Asigna el texto del elemento.
+        mostrarAmigos.appendChild(elementoLista);//Agrega el elemento a la lista.
+    }
+} 
