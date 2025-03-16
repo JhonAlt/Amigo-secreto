5d440128dee388fb11d52240f43ccbc5d0d3f4ea
Jonathan Amézquita <jamezquita150@gmail.com>
2025-03-16 00:38:44 -0600

    Mejorando el código

--- app.js
+++ app.js
@@ -1,93 +1,90 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let nombres = []
-let cantidadMaxAmigos = 10;
-const regex = /^[a-zA-Z\sáéíóúÁÉÍÓÚñÑ]*$/; // Permite letras, espacios y acentos.
+let ListaNombres = []
+const MAX_AMIGOS = 10;
+const NOMBRE_REGEX = /^[a-zA-Z\sáéíóúÁÉÍÓÚñÑ]*$/; // Permite letras, espacios y acentos.
 
 function agregarAmigo() {
-    let nombreAmigo = document.getElementById("amigo").value; //Obtiene el nombre del amigo ingresado.
-    if (nombres.length === cantidadMaxAmigos) { //Verifica si la cantidad de amigos es igual a la cantidad máxima.
+    const nombreAmigo = document.getElementById("amigo").value;//Obtiene el elemento de entrada de texto
+
+    //Validaciones
+    if (ListaNombres.length >= MAX_AMIGOS) { //Verifica si la cantidad de amigos es igual a la cantidad máxima.
         asignarTextoElemento("h2", "Ya no puedes agregar más amigos.")
         limpiar("amigo");
         return;
-    } else {
-        if (nombreAmigo === "") {//Si el nombre del amigo está vacío, muestra un mensaje.
-            Swal.fire({
-                title: 'Campo vacío',
-                text: 'Por favor, ingresa un nombre.',
-                icon: 'warning',
-                confirmButtonText: 'Ok'
-            });
-            return;
-        } 
-        if (nombres.includes(nombreAmigo)) { //Verifica si el nombre ya está incluido en la lista.
-            Swal.fire({
-                title: 'Ya has ingresado este nombre.',
-                text: 'Por favor, ingresa un nombre diferente.',
-                icon: 'warning',
-            });
-            limpiar("amigo");
-            return;
-        }
-        if (!regex.test(nombreAmigo)) { //Verifica si el nombre del amigo es válido.
-            Swal.fire({
-                title: 'Nombre inválido',
-                text: 'Por favor, ingresa un nombre válido.',
-                icon: 'warning',
-            });
-            limpiar("amigo");
-            return;
-        }
-        
     }
-    nombres.push(nombreAmigo)//Agrega el nombre del amigo a la lista.
-
+    if (!nombreAmigo) {//Si el nombre del amigo está vacío, muestra un mensaje.
+        mostrarError("Campo vacío", "Por favor, ingresa un nombre.");
+        return;
+    }
+    if (ListaNombres.includes(nombreAmigo)) { //Verifica si el nombre ya está incluido en la lista.
+        mostrarError("Ya hay un amigo con ese nombre", "Por favor, ingresa un nombre diferente.");
+        limpiar("amigo");
+        return;
+    }
+    if (!NOMBRE_REGEX.test(nombreAmigo)) { //Verifica si el nombre del amigo es válido.
+        mostrarError("Nombre inválido", "Por favor, ingresa un nombre válido.");
+        limpiar("amigo");
+        return;
+    }
+    // Si pasa todas las validaciones, agrega el amigo a la lista.
+    ListaNombres.push(nombreAmigo)
     limpiar("amigo"); //Limpia el cuadro de texto.
-    mostrarLista("listaAmigos", nombres)//Muestra la lista de amigos.
+    mostrarLista("listaAmigos", ListaNombres)//Muestra la lista de amigos.
     cursor();
 }
 
-function asignarTextoElemento(elemento, texto) { 
+// Muestra un mensaje de error utilizando SweetAlert
+function mostrarError(titulo, mensaje) {
+    Swal.fire({
+        title: titulo,
+        text: mensaje,
+        icon: 'warning',
+        confirmButtonText: 'Ok'
+    });
+}
+
+//Asigna un texto a un elemento HTML.
+function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto; 
 }
 
-function limpiar(identificar){//
-    document.getElementById(identificar).value = ""; 
-    document.getElementById(identificar).innerHTML = ""; 
+//Limpia el contenido de un elemento HTML.
+function limpiar(id){//
+    document.getElementById(id).value = "";
+    document.getElementById(id).innerHTML = "";
 }
 
+//Muestra la lista de amigos.
 function mostrarLista(lista, elementos) {
-    let mostrarAmigos = document.getElementById(lista); 
-
+    const mostrarAmigos = document.getElementById(lista);
     mostrarAmigos.innerHTML = ""//Limpia el contenido de la lista.
 
-    for (let i = 0; i < elementos.length; i++) {//Recorre la lista de elementos.
-        let elementoLista = document.createElement("li");//Crea un elemento de lista.
-        elementoLista.textContent = elementos[i];//Asigna el texto del elemento.
-        mostrarAmigos.appendChild(elementoLista);//Agrega el elemento a la lista.
-    }
+   elementos.forEach(elemento => {
+        const li = document.createElement("li");
+        li.textContent = elemento;
+        mostrarAmigos.appendChild(li);
+   });
 } 
 
+//Sortea un amigo aleatoriamente de la lista.
 function sortearAmigo() {
-    if (nombres.length === 0) { //verifica si la lista de amigos está vacía.
-        Swal.fire({
-            title: 'Lista vacía',
-            text: 'Por favor, ingresa al menos dos amigos.',
-            icon: 'warning',
-            confirmButtonText: 'Ok'
-        });
+    if (ListaNombres.length === 0) { //verifica si la lista de amigos está vacía.
+        mostrarError("Lista vacía", "Por favor, agrega al menos dos amigos.");
         return;
     }
-    let amigoSecreto = nombres.splice(Math.floor(Math.random() * nombres.length), 1)[0]; //Selecciona un amigo al azar y lo elimina de la lista.
+    //Selecciona un amigo al azar y lo elimina de la lista.
+    const amigoSecreto = ListaNombres.splice(Math.floor(Math.random() * ListaNombres.length), 1)[0];
     document.getElementById("resultado").innerHTML = `El amigo secreto es: ${amigoSecreto}`
     limpiar("listaAmigos");//Limpia la lista de amigos.
-    if (nombres.length === 0) {
+    if (ListaNombres.length === 0) {
         asignarTextoElemento("h2", "Ya no tienes más amigos para sortear.");
     }
 }
 
+//Resetea la pagina a sus valores iniciales.
 function resetear() {
-    nombres = []
+    ListaNombres = []
     limpiar("listaAmigos");
     asignarTextoElemento("h2", "AGREGA EL NOMBRE DE TUS AMIGOS")
     limpiar("amigo");
